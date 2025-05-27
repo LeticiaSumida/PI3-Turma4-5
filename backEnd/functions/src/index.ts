50c1346be279cbb07b43ed9c8017e0c32b73b301
luccaescova <160538429+luccaescova@users.noreply.github.com>
2025-05-26 23:29:11 -0300

    arquivos iniciais das firebases functions

---
+++ backEnd/functions/src/index.ts
@@ -0,0 +1,13 @@
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export const getRandomNumber = onRequest((request, response) => {
+  response.send("100");
+});
+
+
